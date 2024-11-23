9e02b440bb4c35043b5b71a3a97a56b633b6cc3d
Jeydin21 <JeyPham21@gmail.com>
2024-11-22 18:05:31 -0600

    Start working on importing files from old project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-import { LandingPage } from '@/components/LandingPage';
+import LandingPage from '@/components/LandingPage';
 
 export default function Home() {
   return (
