b0a1f9074d8d1040553ce15a5c527a8a9bd567cb
Jeydin21 <jeydin@j21.dev>
2024-11-23 22:02:56 -0600

    Remove manga

--- components/interface/MainLayout.tsx
+++ components/interface/MainLayout.tsx
@@ -7,24 +7,15 @@ import { useCookies } from 'react-cookie';
 interface MainLayoutProps {
   children: React.ReactNode;
   useHead?: boolean;
-  banner?: string;
-  search?: boolean;
-  landing?: boolean;
-  type?: string;
-  manga?: boolean;
 }
 
-function MainLayout({ children, useHead = true, banner, search = true, landing = false, type, manga = false }: MainLayoutProps) {
+function MainLayout({ children, useHead = true }: MainLayoutProps) {
   const [cookies, setCookie] = useCookies(['theme']);
   const [theme, setTheme] = useState(cookies.theme || 'dark');
 
   useEffect(() => {
     setCookie('theme', theme, { sameSite: 'lax' });
   }, [theme]);
-
-  const toggleTheme = () => {
-    setTheme((prevTheme: string) => prevTheme === 'dark' ? 'light' : 'dark');
-  };
 
   return (
     <div className={`transition-colors duration-200 ${theme === 'dark' ? 'dark' : ''} dark:bg-[#121212] bg-fixed bg-red-500 bg-[radial-gradient(#ffffff90_1px,#f2f2f2_1px)] bg-[size:20px_20px] dark:bg-[radial-gradient(#ffffff15_1px,#121212_1px)]`}>
@@ -60,13 +51,7 @@ function MainLayout({ children, useHead = true, banner, search = true, landing =
         </Head>
       )}
 
-      <Header theme={theme} toggleTheme={toggleTheme} search={search} bg={landing} type={type} manga={manga} />
-      {banner && (
-        <div className="pt-3 relative max-lg:hidden">
-          <img src={banner} className="w-full h-96 object-cover" />
-          <div className="absolute inset-0 bg-gradient-to-b from-transparent dark:to-[#121212] to-[#f2f2f2] opacity-100"></div>
-        </div>
-      )}
+      <Header />
       <div className="flex">
         <div className="z-10 w-full px-5 min-h-[90vh]">
           {children}
