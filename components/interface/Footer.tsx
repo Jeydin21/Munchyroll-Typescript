e6e7d3fa418b7bd9a87ecfe8707bdd7091044ce0
Jeydin21 <jeydin@j21.dev>
2024-11-25 20:19:31 -0600

    Make footer more towards center

--- components/interface/Footer.tsx
+++ components/interface/Footer.tsx
@@ -5,7 +5,7 @@ import { FaGithub, FaDiscord, FaTwitter } from 'react-icons/fa';
 function Footer() {
   return (
     <>
-      <footer className="py-2 px-5 flex flex-col sm:flex-row justify-between">
+      <footer className="py-2 px-5 flex flex-col sm:flex-row justify-around items-center border-t-[2px] border-primary">
         <div className="z-50">
           <Logo />
           <p className="dark:text-primary-light text-primary text-xs pt-2">
@@ -19,8 +19,8 @@ function Footer() {
           </div>
         </div>
       </footer>
-      <footer className="pb-10 px-5">
-        <div className="pt-2 flex justify-between border-t-[2px] border-primary w-full">
+      <footer className="pb-10">
+        <div className="pt-2 flex justify-around items-center w-full">
           <p className="z-50 dark:text-primary-light text-primary text-sm">
             Made with ❤️ by <Link title="Jeydin21" className="hover:text-blue-400 dark:text-secondary text-primary underline transition-all" rel="noreferrer" target="_blank" href="https://j21.dev">Jeydin21</Link>
           </p>
